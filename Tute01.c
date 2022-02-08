930e098cd455506009ee2f88331e7fae86289c6c
IT21301322 <99233029+IT21301322@users.noreply.github.com>
2022-02-08 04:42:20 +0000

    codes

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  float mark1,mark2,avg;
+
+  printf("Input mark of 1st subject : ");
+  scanf("%f",&mark1);
+
+  printf("Input mark of 2nd subject : ");
+  scanf("%f",&mark2);
+
+  avg = (mark1 + mark2) / 2.0;
+
+  printf("The average is %.2f",avg);
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,21 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  float dis,amount;
+
+  printf(" Distance  : ");
+  scanf("%f",&dis);
+
+  if(dis<=30)
+  {
+    amount = dis * 50;
+  }
+  else
+  {
+    amount = 30 * 50 + (dis - 30) * 40;
+  }
+  printf("Amount = %.2f",amount);
   return 0;
 }
